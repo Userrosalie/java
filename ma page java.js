56d9e9a6ff1a3aebeb95ff2fe4d9d4323ffd9760
Rosalie <ngomrosalie@gmail.com>
2023-04-25 16:39:54 +0200

    first commit

---
+++ ma page java.js
@@ -0,0 +1,13 @@
+ //creator "link with html and css"
+ let mybook= { title:"l'histoire de tao",
+    author:"Will Alexander",
+   numberOfPages: 250,
+   isAvailable: true};
+let bookTitle= myBook.Title //"l'histoire de tao"
+let bookPages= myBook.numberOfPages //250
+let totalCDs = 67;
+let totalVinyls = 34;
+let totalMusic = totalCDs + totalVinyls;
+
+
+  
